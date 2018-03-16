7e2ba3f9922efbfea93e93b14cf76a50c49f2fbc
adyd94 <ady.drozman2007@gmail.com>
2018-03-16 15:18:25 +0200

    Add project files.

---
+++ GygTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GygTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GygTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3c621e0-fe77-412f-83dc-40b0c86fbc32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GygTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace GygTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        AppiumDriver<AndroidElement> _driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("deviceName", "Galaxy S6");
+            cap.SetCapability("platformName", "Android");
+            cap.SetCapability("appPackage", "com.guzmanygomez.gyg.uat");
+            cap.SetCapability("appActivity", "com.guzmanygomez.gyg.ui.activities.MainActivity");
+            cap.SetCapability("autoGrantPermissions", "true");
+            cap.SetCapability("noReset", "true");
+            cap.SetCapability("fullReset", "false");
+            cap.SetCapability("VERSION", "6.0.1");
+
+            _driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
+            _driver.FindElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout[2]").Click();
+            _driver.FindElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.RelativeLayout/android.widget.EditText").SendKeys("Newtown");
+            TouchAction action = new TouchAction(_driver);
+            action.Tap(252, 577);
+            action.Release();
+            action.Perform();
+        }
+    }
+}
